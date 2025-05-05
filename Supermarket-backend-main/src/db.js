374cc9ef2a28a0c390a8e5fb53b127e786efa90f
hqunn <143957677+hqunn@users.noreply.github.com>
2025-05-06 01:31:25 +0700

    db.js

--- Supermarket-backend-main/src/db.js
+++ Supermarket-backend-main/src/db.js
@@ -5,7 +5,7 @@ const pool = new Pool({
   user: "postgres",         // Username (default is 'postgres')
   host: "localhost",        // Host (localhost if you're running PostgreSQL locally)
   database: "supermarket",  // The name of the database you created in pgAdmin
-  password: "123Quan123",   // Your PostgreSQL password
+  password: "",   // Your PostgreSQL password
   port: 5432,               // PostgreSQL default port
 });
 
